eb2491bccc7fcb2782f6cca9b3218a611e050a58
菍磐 <qaz123cxd@yeah.net>
2022-11-03 17:04:56 +0800

    Update index.js

--- src/api/user.js
+++ src/api/user.js
@@ -4,20 +4,25 @@ import http from "@/utils/request";
 export const submitUserinfo = (data)=>{
     return http.post('/api/user/addUser',data)
 }
+
+// 获取用户信息
 export const getUserInfo = (params)=>{
     return http.get('/api/user',{
         params
     })
 }
 
+// 修改用户信息
 export const editUser =(data,params)=>{
     return http.post(`/api/user/editUser/${params}`,data)
 }
 
+// 删除用户
 export const deleteUser = (id)=>{
     return http.delete(`/api/user/delete/${id}`)
 }
 
+// 注册用户
 export const loginUser = (data)=>{
     return http.post('/api/user/login',data)
 }
